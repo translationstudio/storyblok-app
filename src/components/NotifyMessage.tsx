f95ebffd306cdc7b90edebcf80af60ee6ad1ecf4
Phillip Austerfield <phillip.austerfield@idmedia.com>
2025-07-09 09:37:58 +0100

    change ui

--- src/components/NotifyMessage.tsx
+++ src/components/NotifyMessage.tsx
@@ -17,37 +17,12 @@ along with this program; if not, see https://www.gnu.org/licenses/old-licenses/g
 */
 import React from "react";
 import { NotifyMessage as NotifyMessageType } from '@/interfaces_types';
-import  {Box, Typography, }  from '@mui/material'
-import { Done, ErrorOutline, WarningAmber, InfoOutlined } from '@mui/icons-material/';
+import  {Alert }  from '@mui/material'
 
-type NotifyMessageProps = {
-    notifyMessage: NotifyMessageType;
-}
-
-const Icon = function(props:{ notifyMessage:NotifyMessageType }) 
-{        
-    if (props.notifyMessage?.withIcon !== true)
-        return
-
-    switch(props.notifyMessage.type) {
-        case "success":
-            return <Done sx={{mr:2}}/>
-        case "warning":
-            return <WarningAmber sx={{mr:2}}/>
-        case "error":
-            return <ErrorOutline sx={{mr:2}}/>     
-        default:
-            return <InfoOutlined sx={{mr:2}}/>
-    }
-}
-export default function NotifyMessage({notifyMessage}:NotifyMessageProps) 
+export default function NotifyMessage(props: {notifyMessage:NotifyMessageType}) 
 {
-    if ( !notifyMessage ) 
+    if ( !props.notifyMessage || !props.notifyMessage.message) 
         return <></>    
     
-    const customBgColor = notifyMessage.type ? notifyMessage.type + '.main' : "primary.main";
-    return <Box color='white' sx={{display: notifyMessage.message ?  'flex' : 'none', mr:2, mb:2, pt:1, pl:2, pr:2, borderRadius: 1, bgcolor: customBgColor}}>                
-        <Icon notifyMessage={notifyMessage} />
-        <Typography  variant="body1" gutterBottom>{notifyMessage.message}</Typography>
-    </Box>;
+    return <Alert severity={props.notifyMessage.type ?? "info"} style={{ marginBottom: "2em" }}>{props.notifyMessage.message}</Alert>
 }
