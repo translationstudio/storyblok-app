38675f04a8f68ca9f9f59506332cb2ad3f328584
Phillip Austerfield <phillip.austerfield@idmedia.com>
2025-07-11 10:55:45 +0100

    change height

--- src/utils/Logo.tsx
+++ src/utils/Logo.tsx
@@ -4,7 +4,7 @@ const TSLOGO = "/logo/ts-logo-colored.svg";
 export default function TranslationstudioLogo()
 {
     return <Grid item xs={12} textAlign={"center"} style={{ paddingBottom: "4em"}}>
-            <img className="translationstudiologo" src={TSLOGO} alt="" style={{ width: "100%", maxHeight: "150px", height: "100%"}}/>
+            <img className="translationstudiologo" src={TSLOGO} alt="" style={{ width: "100%", maxHeight: "100px", height: "100%"}}/>
         </Grid>
     
 }
